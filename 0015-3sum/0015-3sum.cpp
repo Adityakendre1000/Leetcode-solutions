936b36ca083998909f29e167f6fbcb205d8bf1e7
Adityakendre1000 <130606499+Adityakendre1000@users.noreply.github.com>
2024-06-03 02:31:13 +0530

    Time: 63 ms (64.97%), Space: 27 MB (93.81%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,35 +7,35 @@ public:
         
         vector<vector<int>> result;
         
-        int right = n-1;
         
         for (int i = 0; i < n - 2; ++i) {
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
             int left = i + 1;
-            int right = n - 1;
+            int right = n-1;
             
-            while (left < right) {
+            while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
                 
-                if (sum == 0) {
-                    result.push_back({nums[i], nums[left], nums[right]});
-
-                    while (left < right && nums[left] == nums[left + 1]) ++left;
-
-                    while (left < right && nums[right] == nums[right - 1]) --right;
+                if(sum == 0){
+                    
+                    result.push_back({nums[i],nums[left],nums[right]});
+                    
+                    while(left < right && nums[left] == nums[left+1]) ++left;
+                    
+                    while(left < right && nums[right] == nums[right-1]) --right;
                     
                     ++left;
                     --right;
-                } else if (sum < 0) {
-                    ++left;
-                } else {
+                }
+                else if(sum > 0){
                     --right;
+                }
+                else{
+                    ++left;
                 }
             }
         }
-        
         return result;
-        
     }
 };
