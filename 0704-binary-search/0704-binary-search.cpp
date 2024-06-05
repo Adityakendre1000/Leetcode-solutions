91200072e5e450ea3fd2b6b69a6b375631663c8a
Aditya Kendre <130606499+Adityakendre1000@users.noreply.github.com>
2024-06-06 03:11:55 +0530

    Time: 22 ms (86.71%), Space: 29.7 MB (99.40%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,15 +5,11 @@ public:
         int right = nums.size()-1;
         
         while(left <= right){
-            int mid = (left+right)/2;
+            int mid = left + (right-left)/2;
             
             if(nums[mid] == target) return mid;
-            else if(nums[mid] > target){
-                right = mid-1;
-            }
-            else{
-                left = mid +1;
-            }
+            else if(nums[mid] > target) right = mid-1;
+            else left = mid +1;
         }
         return -1;
     }
