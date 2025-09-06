6bb5d7095510958ee855a4537ac12af227cc7e34
Aditya Kendre <130606499+Adityakendre1000@users.noreply.github.com>
2025-09-06 18:39:04 +0530

    Time: 50 ms (15.05%), Space: 25.3 MB (5.02%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -15,36 +15,47 @@ public:
         ListNode* x = headA;
         ListNode* y = headB;
 
-        unordered_set<ListNode*> visited;
+        // unordered_set<ListNode*> visited;
 
-        while(x && y){
-            if(visited.find(x) != visited.end()) return x;
-            visited.insert(x);
-            x = x->next;
-            if(visited.find(y) != visited.end()) return y;
-            visited.insert(y);
-            y = y->next;
-            // else{
-            //     visited.insert(x);
-            //     visited.insert(y);
-            //     x = x->next;
-            //     y = y->next;
-            // }
-        }
+        // while(x && y){
+        //     if(visited.find(x) != visited.end()) return x;
+        //     visited.insert(x);
+        //     x = x->next;
+        //     if(visited.find(y) != visited.end()) return y;
+        //     visited.insert(y);
+        //     y = y->next;
+        //     // else{
+        //     //     visited.insert(x);
+        //     //     visited.insert(y);
+        //     //     x = x->next;
+        //     //     y = y->next;
+        //     // }
+        // }
         
-        while(x){
-            if(visited.find(x) != visited.end()) return x;
-            else{
-                visited.insert(x);
-                x = x->next;
-            }
+        // while(x){
+        //     if(visited.find(x) != visited.end()) return x;
+        //     else{
+        //         visited.insert(x);
+        //         x = x->next;
+        //     }
+        // }
+        // while(y){
+        //     if(visited.find(y) != visited.end()) return y;
+        //     else{
+        //         visited.insert(y);
+        //         y = y->next;
+        //     }
+        // }
+
+        unordered_map<ListNode*,int> visited;
+        while (x) {
+            visited[x] = 1;
+            x = x->next;
         }
-        while(y){
-            if(visited.find(y) != visited.end()) return y;
-            else{
-                visited.insert(y);
-                y = y->next;
-            }
+
+        while (y) {
+            if (visited[y] == 1) return y;  // found intersection
+            y = y->next;
         }
 
         return NULL;
