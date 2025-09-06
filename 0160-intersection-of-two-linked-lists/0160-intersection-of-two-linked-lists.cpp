b0ea211f40907bfb7330f9c1a8c2f7fefaf09792
Aditya Kendre <130606499+Adityakendre1000@users.noreply.github.com>
2025-09-06 18:11:45 +0530

    Time: 45 ms (20.77%), Space: 23.9 MB (5.25%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -17,16 +17,20 @@ public:
 
         unordered_set<ListNode*> visited;
 
-        // while(x && y){
-        //     if(visited.find(x) != visited.end()) return x;
-        //     else if(visited.find(y) != visited.end()) return y;
-        //     else{
-        //         visited.insert(x);
-        //         visited.insert(y);
-        //         x = x->next;
-        //         y = y->next;
-        //     }
-        // }
+        while(x && y){
+            if(visited.find(x) != visited.end()) return x;
+            visited.insert(x);
+            x = x->next;
+            if(visited.find(y) != visited.end()) return y;
+            visited.insert(y);
+            y = y->next;
+            // else{
+            //     visited.insert(x);
+            //     visited.insert(y);
+            //     x = x->next;
+            //     y = y->next;
+            // }
+        }
         
         while(x){
             if(visited.find(x) != visited.end()) return x;
