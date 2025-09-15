53a025e205d207742d4c68c1b5781934be503a87
Aditya Kendre <130606499+Adityakendre1000@users.noreply.github.com>
2025-09-15 16:21:53 +0530

    Time: 337 ms (63.07%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+select case
+when id % 2 = 1 and id+1 in(select id from seat) then id+1
+when id % 2  = 0 then id-1
+else id
+end as id,student
+from seat
+order by id;
