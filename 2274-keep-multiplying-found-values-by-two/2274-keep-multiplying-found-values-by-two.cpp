74aee958a70dd3f18cae430e077c733d6dc4bae7
Aditya Kendre <130606499+Adityakendre1000@users.noreply.github.com>
2025-11-19 17:46:52 +0530

    Time: 4 ms (31.07%), Space: 15.9 MB (20%) - LeetHub

---
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> myMap;
+        for(int num:nums){
+            myMap.insert(num);
+        }
+        while(myMap.find(original) != myMap.end()) original*=2;
+
+        return original;
+
+    }
+};
