2088830856481ba06a460f938de198c2cb2122d1
Aditya Kendre <130606499+Adityakendre1000@users.noreply.github.com>
2025-11-19 17:53:39 +0530

    Time: 4 ms (31.07%), Space: 16.1 MB (6.93%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
         unordered_set<int> myMap;
         for(int num:nums){
             myMap.insert(num);
