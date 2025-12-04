8053340cb768883d0424955bfb911b9242a28819
Aditya Kendre <130606499+Adityakendre1000@users.noreply.github.com>
2025-12-04 18:18:48 +0530

    Time: 10 ms (68.12%), Space: 19.2 MB (84.64%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,76 +1,28 @@
 class Solution {
 public:
-    int countCollisions(string directions) {
-        int c = 0;
-        int n = directions.size()-1;
-        // while(directions[0]=='L') directions.erase(0,1);
-        // while(directions[n]=='R'){ 
-        //     directions.pop_back();
-        //     n--;
-        // }
+    int countCollisions(std::string directions) {
+        int n = directions.length();
+        if (n < 2) return 0;
 
-
-        // for(int i = 0; i < directions.size()-1;i++){
-        //     if(directions[i]=='R' && directions[i+1]=='L'){
-        //         c+=2;
-        //         directions[i]='S'; 
-        //         directions[i+1]='S';
-        //     } else if(directions[i]=='R' && directions[i+1]=='S'){
-        //         c+=1;
-        //         directions[i]='S';
-        //     } else if(directions[i]=='S' && directions[i+1]=='L'){
-        //         c+=1;
-        //         directions[i+1]='S';
-        //     } else{
-        //         c+=0;
-        //     }
-        // }
-
-        bool x = true;
-        while (x&&n>0){
-            while(n>-1 && directions.front() == 'L'){ 
-                directions.erase(0,1);
-                n--;    
-            }
-            while(n>-1 && directions.back() == 'R'){ 
-                directions.pop_back();
-                n--;
-            }
-            int cr = 0;
-            int b = 0;
-
-            for(int i = 0; i < n;i++){
-                if(directions[i]=='R' && directions[i+1]=='L'){
-                    c+=2;
-                    b++;
-                    directions[i]='S'; 
-                    directions[i+1]='S';
-                } else if(directions[i]=='R' && directions[i+1]=='S'){
-                    c+=1;
-                    directions[i]='S';
-                    b++;
-                } else if(directions[i]=='S' && directions[i+1]=='L'){
-                    c+=1;
-                    directions[i+1]='S';
-                    b++;
-                } else{
-                    c+=0;
-                }
-
-                //cosecutive R
-                if(directions[i]=='R'){
-                    cr++;
-                } else if(cr>0){
-                    c+=cr;
-                    // cr=0;
-
-                    for(cr; cr > 0; cr--) directions[i-cr] = 'S';
-                }
-            }
-            if(b==0) x = false;
-            x=false;
-
+        int left = 0;
+        while (left < n && directions[left] == 'L') {
+            left++;
         }
-        return c;
+
+        int right = n - 1;
+        while (right >= 0 && directions[right] == 'R') {
+            right--;
+        }
+        
+        if (left >= right) return 0;
+
+        int collisions = 0;
+        for (int i = left; i <= right; ++i) {
+            if (directions[i] != 'S') {
+                collisions++;
+            }
+        }
+        
+        return collisions;
     }
 };
